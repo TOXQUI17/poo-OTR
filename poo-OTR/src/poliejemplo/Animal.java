55f435adda1dcb01ea6c8cc5f4dccd45742cd65d
T-102 <T-102@PC259>
2018-02-12 19:46:42 -0600

    Poliformismo

---
+++ poo-OTR/src/poliejemplo/Animal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poliejemplo;
+
+/**
+ *
+ * @author T-102
+ */
+public class Animal {
+    String color;
+    int edad;
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    
+}
